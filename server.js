b86a4644905493364a8d8a25978cb2be4ae01e34
GPV <gvill005@gmail.com>
2024-10-18 11:19:54 -0700

    clean up unnecessary code

--- server.js
+++ server.js
@@ -8,144 +8,6 @@ const app = express();
 dotenv.config();
 const PORT = process.env.PORT || 3000;
 
-// const data = [
-//   {
-//     name: '#1 Natural Juice 16oz',
-//     properties: '[\n' +
-//       '  {\n' +
-//       '    "name": "_tpo_main_product_id",\n' +
-//       '    "value": "48780060688671"\n' +
-//       '  },\n' +
-//       '  {\n' +
-//       '    "name": "_tpo_add_on_key",\n' +
-//       '    "value": "ff090bcf-30ab-42fc-b557-f64847a5c7d1"\n' +
-//       '  }\n' +
-//       ']'
-//   },
-//   {
-//     name: 'Extra Guacamole',
-//     properties: '[\n' +
-//       '  {\n' +
-//       '    "name": "_tpo_main_product_id",\n' +
-//       '    "value": "48780060688671"\n' +
-//       '  },\n' +
-//       '  {\n' +
-//       '    "name": "_tpo_add_on_key",\n' +
-//       '    "value": "c2851812-895d-4796-bfe6-0707d2437dba"\n' +
-//       '  }\n' +
-//       ']'
-//   },
-//   {
-//     name: 'Green Salsa',
-//     properties: '[\n' +
-//       '  {\n' +
-//       '    "name": "_tpo_main_product_id",\n' +
-//       '    "value": "48780060688671"\n' +
-//       '  },\n' +
-//       '  {\n' +
-//       '    "name": "_tpo_add_on_key",\n' +
-//       '    "value": "b9b29955-8325-49fc-bcd6-f91cdea58cd2"\n' +
-//       '  }\n' +
-//       ']'
-//   },
-//   {
-//     name: 'Brisket',
-//     properties: '[\n' +
-//       '  {\n' +
-//       '    "name": "_tpo_main_product_id",\n' +
-//       '    "value": "48780060688671"\n' +
-//       '  },\n' +
-//       '  {\n' +
-//       '    "name": "_tpo_add_on_key",\n' +
-//       '    "value": "3a60c642-3a4d-4df7-9d07-1c9e39321dfe"\n' +
-//       '  }\n' +
-//       ']'
-//   },
-//   {
-//     name: 'Breakast Burrito',
-//     properties: '[\n' +
-//       '  {\n' +
-//       '    "name": "_tpo_add_on_keys",\n' +
-//       '    "value": "[\\"3a60c642-3a4d-4df7-9d07-1c9e39321dfe\\",\\"b9b29955-8325-49fc-bcd6-f91cdea58cd2\\",\\"c2851812-895d-4796-bfe6-0707d2437dba\\",\\"ff090bcf-30ab-42fc-b557-f64847a5c7d1\\"]"\n' +
-//       '  },\n' +
-//       '  {\n' +
-//       '    "name": "_tpo_add_on_variant_ids",\n' +
-//       '    "value": "[\\"49087357681951\\",\\"49085860380959\\",\\"49085834821919\\",\\"48839638352159\\"]"\n' +
-//       '  },\n' +
-//       '  {\n' +
-//       '    "name": "Meat Options",\n' +
-//       '    "value": "Upgrade to Brisket ( +$3.49 )"\n' +
-//       '  },\n' +
-//       '  {\n' +
-//       '    "name": "Salsa",\n' +
-//       '    "value": "Green Salsa (mild)"\n' +
-//       '  },\n' +
-//       '  {\n' +
-//       '    "name": "Extra Toppings",\n' +
-//       '    "value": "Guacamole ( +$2.00 )"\n' +
-//       '  },\n' +
-//       '  {\n' +
-//       '    "name": "Drinks:",\n' +
-//       '    "value": "#1 Natural Juice 16oz ( +$6.49 )"\n' +
-//       '  },\n' +
-//       '  {\n' +
-//       '    "name": "_tpo_is_main_product",\n' +
-//       '    "value": "1"\n' +
-//       '  }\n' +
-//       ']'
-//   },
-//   { name: 'Tip', properties: '[]' }
-// ]
-
-// // orderDetails -> line_items -> loop through properties for each product -> look for '_tpo_is_main_product' -> create print of add-ons within this main product
-
-// // loop through line items X
-//   // get order data.line_items
-//   // loop through line_items array.
-// // in each line_item index, loop thorugh products properties
-// // confirm main product
-// // create print
-// // continue looping through line items
-
-// // send line items data in our print middlewate
-// const lineItems = orderData.line_items; // SOURCE FROMS HOPIFY WEBHOOK. Arrives to printer middelware as inside 'orderDetails' object package.
-
-// // now we are in Printer function
-// const orderLineItems = orderDetails.lineItems;
-// // loop through line items and loop through properties
-// const loopThroughItems = (orderLineItems) => {
-//   // what do we do when we find the main product?
-//   // loop through ine items,
-//   for(let i = 0; i < orderLineItems.length; i++){
-//     // confirm main product with a boolean function
-//     if(isMainProduct(orderLineItems[i])){
-
-//     }
-
-//   }
-// };
-
-
-// // looping through product's properties to find '_tpo_is_main_product'
-// const isMainProduct = (product) => {
-//   for(let i = 0; i < product.length; i++){
-//     if(product[i].properties === '_tpo_is_main_product')
-//   }
-// };
-
-// ${orderDetails.lineItems.map(item => {
-//   // through each item, 
-//   // Pad the item name to ensure alignment
-//   const itemLine = `${item.quantity} x ${item.name}`;
-//   const priceLine = ` - $${item.unitPrice}`;
-//   return `${itemLine}${priceLine}`;
-// }).join('\n')}
-
-
-
-
-// console.log("TEST: ", test(data))
-
 
 // Middleware to capture the raw body before JSON parsing
 app.use(express.json({
@@ -184,13 +46,9 @@ async function printOrder(orderDetails) {
         }).join('');
 
         return itemLine + addOnLines;
-        // const priceLine = ` - $${item.unitPrice}`;
 
-        // return `${itemLine}${priceLine}`;
-    }).join('')}\n------------------------------\nNote: ${orderDetails.note}\nSubtotal: $${orderDetails.subtotal}\nDiscount: -$${orderDetails.discount}\nTip: $${orderDetails.tipReceived}\nTaxes: $${orderDetails.tax}\n------------------------------\nTotal: $${orderDetails.totalPrice}\n------------------------------\nPayment Method: ${orderDetails.paymentMethod}\nPaid: ${orderDetails.paid ? 'Yes' : 'No'}\n\n\n`;
+    }).join('')}------------------------------\nNote: ${orderDetails.note}\nSubtotal: $${orderDetails.subtotal}\nDiscount: -$${orderDetails.discount}\nTip: $${orderDetails.tipReceived}\nTaxes: $${orderDetails.tax}\n------------------------------\nTotal: $${orderDetails.totalPrice}\n------------------------------\nPayment Method: ${orderDetails.paymentMethod}\nPaid: ${orderDetails.paid ? 'Yes' : 'No'}\n\n\n`;
 
-    // console.log("Printing content:", printContent);
-    // console.log("Print order function confirmed");
 
     try {
       const response = await axios.post(
@@ -220,12 +78,6 @@ app.post('/shopify-order-webhook', verifyShopifyWebhook, async (req, res) => {
     try{
     const orderData = req.body;
 
-    // console.log("Examine Data!: " , orderData.line_items.map(item => ({
-    //   name: item.title,
-    //   properties: JSON.stringify(item.properties, null, 2)
-    // })));
-
-    // console.log("Order Data: ", orderData);
     // date for receipt
     const date = new Date(orderData.created_at);
     const formattedDate = date.toLocaleString('en-US', {
@@ -239,8 +91,7 @@ app.post('/shopify-order-webhook', verifyShopifyWebhook, async (req, res) => {
     });
 
     // Extract necessary information from Shopify order data
-    // const orderId = orderData.id;
-    const orderNumber = orderData.order_number;
+\    const orderNumber = orderData.order_number;
     const createdAt = formattedDate;
     const pickupTime = (orderData.note_attributes[6] && orderData.note_attributes[6].value !== undefined ) ? orderData.note_attributes[6].value : '';
     const pickupDay = (orderData.note_attributes[1] && orderData.note_attributes[1].value !== undefined ) ? orderData.note_attributes[1].value : '';
@@ -249,8 +100,7 @@ app.post('/shopify-order-webhook', verifyShopifyWebhook, async (req, res) => {
     const customerPhone = orderData.customer.phone ? orderData.customer.phone : '';
 
     const lineItems = orderData.line_items.reduce((result, item) => {
-        // Parse properties data
-        // const properties = JSON.parse(item.properties);
+
         const properties = item.properties;
 
         // check if at leas one element passes given test (returns true)
@@ -274,7 +124,6 @@ app.post('/shopify-order-webhook', verifyShopifyWebhook, async (req, res) => {
           console.log('itemmm: ', item.properties)
 
           orderData.line_items.forEach(addOnItem => {
-            // const addOnProperties = JSON.parse(addOnItem.properties);
             const addOnProperties = addOnItem.properties;
 
             const addOnKey = addOnProperties.find(prop => prop.name === '_tpo_add_on_key')?.value;
@@ -296,8 +145,6 @@ app.post('/shopify-order-webhook', verifyShopifyWebhook, async (req, res) => {
         return result;
       }, []);
     
-      console.log("WHat are the line Items?: ", lineItems);
-
     const note = orderData.note || '';
     const tipReceived = orderData.total_tip_received || '0.00';
     const discount = orderData.total_discounts || '0.00';
