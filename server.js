77deb51566849c162d75dcc3e630ae356b1a7c33
GPV <gvill005@gmail.com>
2025-01-08 13:32:44 -0800

    got rid or syntax error to test shopify webhook

--- server.js
+++ server.js
@@ -91,7 +91,7 @@ app.post('/shopify-order-webhook', verifyShopifyWebhook, async (req, res) => {
     });
 
     // Extract necessary information from Shopify order data
-\    const orderNumber = orderData.order_number;
+    const orderNumber = orderData.order_number;
     const createdAt = formattedDate;
     const pickupTime = (orderData.note_attributes[6] && orderData.note_attributes[6].value !== undefined ) ? orderData.note_attributes[6].value : '';
     const pickupDay = (orderData.note_attributes[1] && orderData.note_attributes[1].value !== undefined ) ? orderData.note_attributes[1].value : '';
