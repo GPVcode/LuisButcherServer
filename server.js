6c7b357afba95e3eb26dc208ccfc028e5862393b
Gabriel Villaruel <gvill005@gmail.com>
2025-01-20 16:45:34 -0800

    cleaned up console responses

--- server.js
+++ server.js
@@ -73,7 +73,7 @@ async function printOrder(orderDetails) {
       console.error('Error sending print job:', error.response ? error.response.data : error.message);
     }
   }
-
+  
 app.post('/shopify-order-webhook', verifyShopifyWebhook, async (req, res) => {
     try{
     const orderData = req.body;
@@ -92,12 +92,12 @@ app.post('/shopify-order-webhook', verifyShopifyWebhook, async (req, res) => {
 
     // Extract necessary information from Shopify order data
     const orderNumber = orderData.order_number;
-    console.log("Order Data: ", orderData.note_attributes)
+    // console.log("Order Data: ", orderData.note_attributes)
     const createdAt = formattedDate;
     const pickupTime = (orderData.note_attributes[6] && orderData.note_attributes[6].value !== undefined ) ? orderData.note_attributes[6].value : '';
     // const pickupDay = (orderData.note_attributes[1] && orderData.note_attributes[1].value !== undefined ) ? orderData.note_attributes[4].value : '';
     const pickupDay = orderData.note_attributes.find(attr => attr.name === 'Delivery Date')?.value || '';
-    console.log("Pick up day", pickupDay)
+    // console.log("Pick up day", pickupDay)
     const customerName = `${orderData.customer.first_name} ${orderData.customer.last_name}`;
     const customerEmail = orderData.customer.email;
     const customerPhone = orderData.customer.phone ? orderData.customer.phone : '';
